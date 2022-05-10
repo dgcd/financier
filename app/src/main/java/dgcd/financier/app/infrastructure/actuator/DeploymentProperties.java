a6d04cfcb268367f99976a2f897d59afe9cffbde
Dmitry <47191419+dgcd@users.noreply.github.com>
2022-05-10 17:24:24 +0300

    Added Spring Actuator (#8)
    * added actuator
    
    * fixes
    
    * fixes

--- app/src/main/java/dgcd/financier/app/infrastructure/actuator/DeploymentProperties.java
+++ app/src/main/java/dgcd/financier/app/infrastructure/actuator/DeploymentProperties.java
@@ -1,4 +1,4 @@
-package dgcd.financier.app.modules.initData;
+package dgcd.financier.app.infrastructure.actuator;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
