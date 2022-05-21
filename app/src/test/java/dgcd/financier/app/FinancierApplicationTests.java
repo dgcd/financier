a36e917d776e7d99df1ab9603b0891820d38e454
Dmitry <47191419+dgcd@users.noreply.github.com>
2022-05-22 01:21:56 +0300

    Database creation refactoring and first tests (#10)

--- app/src/test/java/dgcd/financier/app/FinancierApplicationTests.java
+++ app/src/test/java/dgcd/financier/app/FinancierApplicationTests.java
@@ -3,6 +3,9 @@ package dgcd.financier.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * This test loads whole context and distorts coverage
+ */
 @SpringBootTest
 class FinancierApplicationTests {
 
