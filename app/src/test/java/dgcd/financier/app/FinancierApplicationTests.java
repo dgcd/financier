f092fddd458d1fda076a19b1a611cb00e30f803b
dgcd <dgnedin@mail.ru>
2022-05-21 16:56:09 +0300

    tests

--- app/src/test/java/dgcd/financier/app/FinancierApplicationTests.java
+++ app/src/test/java/dgcd/financier/app/FinancierApplicationTests.java
@@ -1,8 +1,10 @@
 package dgcd.financier.app;
 
+import dgcd.financier.app.test.FinancierTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@FinancierTest
 @SpringBootTest
 class FinancierApplicationTests {
 
