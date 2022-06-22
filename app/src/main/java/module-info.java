553a3786b83b7c3a3d6cc660b8a0913c99e657a5
dgcd <dgnedin@mail.ru>
2022-06-22 23:04:57 +0300

    added module-info

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+module dgcd.financier.app {
+    requires spring.core;
+    requires spring.web;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.tx;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.boot.actuator;
+    requires spring.data.jpa;
+
+    requires jakarta.validation;
+    requires jakarta.persistence;
+
+    requires org.aspectj.weaver;
+    requires org.hibernate.orm.core;
+    requires org.apache.tomcat.embed.core;
+    requires org.apache.poi.ooxml;
+    requires com.fasterxml.jackson.annotation;
+
+    requires lombok;
+}
