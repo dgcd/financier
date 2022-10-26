a72ba9e1cb8ead958481d3c1f4161458d006bf99
dgcd <dgnedin@mail.ru>
2022-10-26 23:14:53 +0300

    Virtual threads for spring boot

--- app/src/main/java/dgcd/financier/app/infrastructure/web/WebConfig.java
+++ app/src/main/java/dgcd/financier/app/infrastructure/web/WebConfig.java
@@ -1,11 +1,17 @@
 package dgcd.financier.app.infrastructure.web;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
 
+import java.util.concurrent.Executors;
+
+import static org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Configuration
@@ -16,6 +22,16 @@ public class WebConfig {
         return container -> container.addErrorPages(
                 new ErrorPage(NOT_FOUND, "/")
         );
+    }
+
+    @Bean(APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+    public AsyncTaskExecutor asyncTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+        return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
     }
 
 }
