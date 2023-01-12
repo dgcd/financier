829e9c928ff4e12c0ab611ddc8688cb70bbce3b1
Dmitry <47191419+dgcd@users.noreply.github.com>
2023-01-12 21:16:04 +0300

    Front: categories count and accounts view refactoring (#30)

--- front/src/views/Accounts/AccountsTable.vue
+++ front/src/views/Accounts/AccountsTable.vue
@@ -2,7 +2,7 @@
     <div>
         <table class="tbl">
             <tr>
-                <th>Id</th>
+                <!-- <th>Id</th> -->
                 <th>Title</th>
                 <th>Currency</th>
                 <th>Balance</th>
@@ -10,7 +10,7 @@
                 <th v-for="c in getHistoryColumns" :key="c">{{c}}</th>
             </tr>
             <tr v-for="a in sortedAccounts" :key="a.id">
-                <td>{{ a.id }}</td>
+                <!-- <td>{{ a.id }}</td> -->
                 <td align="left" style="min-width:250px">
                     {{ a.title }}
                     <button class="btn btn-link btn-sm py-0" v-if="a.id && !a.balance && !a.isClosed" @click="closeAccountHandler(a)">X</button>
@@ -25,8 +25,9 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 import dicts from '@/config/dicts.js';
+import utils from '@/service/utils.js';
 
 export default {
     name: 'Accounts-AccountsTable',
@@ -107,21 +108,20 @@ export default {
         },
 
         getHistoryColumns() {
-            const colsSet = new Set();
-            for (let op of this.operations) {
-                const col = this.dateToHistoryColumn(op.date);
-                colsSet.add(col);
+            const colsArray = [];
+            for (let year of this.makeMonthsSpace()) {
+                for (let month of year.monthsTokens) {
+                    colsArray.push(month);
+                }
             }
-            const colsArray = [...colsSet].sort((s1,s2) => s2.localeCompare(s1));
+            colsArray.reverse();
             colsArray.push('BASE');
             return colsArray;
         },
     },
 
     methods: {
-        dateToHistoryColumn(date) {
-            return date.substring(5,7) + "/" + date.substring(2,4);
-        },
+        ...mapGetters(['makeMonthsSpace']),
 
         getHistoryValue(account, column) {
             if (!account.id) {
@@ -133,7 +133,7 @@ export default {
                     if (column === 'BASE') {
                         return column === op.type;
                     }
-                    return column.localeCompare(this.dateToHistoryColumn(op.date)) >= 0;
+                    return column === utils.getMonthTokenByIsoDate(op.date);
                 })
                 .map(op => op.amount)
                 .reduce((amount, accumulator) => amount + accumulator, 0);
