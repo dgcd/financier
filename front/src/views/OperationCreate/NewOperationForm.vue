0a80b538231304709f9e9b4ff3c22387222fad67
dgcd <dgnedin@mail.ru>
2023-04-07 22:07:34 +0300

    expressions phase 1

--- front/src/views/OperationCreate/NewOperationForm.vue
+++ front/src/views/OperationCreate/NewOperationForm.vue
@@ -135,7 +135,7 @@ export default {
 
     computed: {
         price() {
-            return Number(this.amount) / Number(this.quantity);
+            return Number(this.parseAmount(this.amount)) / Number(this.quantity);
         },
 
         showCategories() {
@@ -165,7 +165,8 @@ export default {
         emitOperation() {
             if (!this.emitEnabled) return;
             const qtty = Number(this.quantity);
-            const amnt = Number(this.amount);
+            const amnt = this.parseAmount(this.amount);
+            console.log(`amnt: ${amnt}`);
             const amntTo = Number(this.amountTo);
             const operation = {
                 date: this.date,
@@ -199,6 +200,105 @@ export default {
             };
             const cleanedOp = utils.removeEmptyFieldsFromObject(operation);
             this.$emit('input', cleanedOp);
+        },
+
+        parseAmount(amount) {
+            if (amount === null || amount === undefined) {
+                console.log(`undef: ${amount}`);
+                return;
+            }
+            let noSpaces = amount.replace(/\s+/g, '');
+            console.log(`noSpaces: ${noSpaces}`);
+
+            if (noSpaces.length > 0 && noSpaces.charAt(0) === '=') {
+                return this.parseAmountExpression(noSpaces.substring(1));
+            }
+
+            let result = Number(noSpaces);
+            console.log(`result: ${result}`);
+
+            if (Number.isNaN(result)) {
+                console.log(`isNaN: ${result}`);
+            }
+            return result;
+        },
+
+        isalnum(str) {
+            var code, i, len;
+
+            for (i = 0, len = str.length; i < len; i++) {
+                code = str.charCodeAt(i);
+                if (!(code > 47 && code < 58)
+                    //  && // numeric (0-9)
+                    // !(code > 64 && code < 91) && // upper alpha (A-Z)
+                    // !(code > 96 && code < 123)
+                    ) { // lower alpha (a-z)
+                    return false;
+                }
+            }
+            return true;
+        },
+
+        parseAmountExpression(s) {
+            let st = [];
+            let op = [];
+            for (let i = 0; i < s.length; i++) {
+            //   console.log(i);
+                // if (s.charAt(i) == '(') {
+                //     op.push('(');
+                // } else if (s.charAt(i) == ')') {
+                //     while (op[op.length -1] != '(') {
+                //         this.process_op(st, op[op.length -1]);
+                //         op.pop();
+                //     }
+                //     op.pop();
+                // } else 
+                if (this.is_op(s.charAt(i))) {
+                    let cur_op = s.charAt(i);
+                    while (op.length !== 0 && this.priority(op[op.length -1]) >= this.priority(cur_op)) {
+                        this.process_op(st, op[op.length -1]);
+                        op.pop();
+                    }
+                    op.push(cur_op);
+                } else {
+                    let number = 0;
+                    while (i < s.length && this.isalnum(s.charAt(i)))
+                        number = number * 10 + s[i++];// - '0';
+                    --i;
+                    st.push(number);
+                }
+            }
+            console.log(st);
+            console.log(op);
+
+            while (op.length !== 0) {
+                this.process_op(st, op[op.length -1]);
+                op.pop();
+            }
+            return st[st.length -1];
+        },
+
+        priority (op) {
+            if (op === '+' || op === '-')
+                return 1;
+            if (op === '*' || op === '/')
+                return 2;
+            return -1;
+        },
+
+        is_op(c) {
+            return c === '+' || c === '-' || c === '*' || c === '/';
+        },
+
+        process_op(st, op) {
+            let r = st.pop();
+            let l = st.pop();
+            switch (op) {
+                case '+': st.push(l + r); break;
+                case '-': st.push(l - r); break;
+                case '*': st.push(l * r); break;
+                case '/': st.push(l / r); break;
+            }
         },
     },
 }
