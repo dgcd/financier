c8db5212dca27787583dd745055cb65cdc27e4e3
dgcd <dgnedin@mail.ru>
2023-11-22 21:39:58 +0300

    back deps upd

--- financier-app/src/test/java/dgcd/financier/test/integration/ZonkyTest.java
+++ financier-app/src/test/java/dgcd/financier/test/integration/ZonkyTest.java
@@ -3,6 +3,7 @@ package dgcd.financier.test.integration;
 import dgcd.financier.test.integration.config.FinancierIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,13 +14,16 @@ public class ZonkyTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Value("${app.test.database.version-string}")
+    private String databaseVersion;
+
 
     @Test
     void test_checkPostgresVersion_OK() {
         var version = jdbcTemplate.queryForObject("select version();", String.class);
 
         assertThat(version).isNotNull();
-        assertThat(version).contains("PostgreSQL 16.0");
+        assertThat(version).contains(databaseVersion);
     }
 
 }
